ca8ccc032cd415eb09bdb960d99b93bbf3ff8c6d
Matt Green <magreen118@gmail.com>
2020-05-18 16:36:24 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -8,3 +8,11 @@ end
 puts "Code after if...end"
 =end
 
+chance_of_rain = 0.2
+puts "Let's go outside!"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy tje fine day!"
+end
+puts "oh, and always wear sunscreeen!"
